cc38f138373bb920dd3b3f3bed6554ffad484a73
dringib1-hub <dringib1@gmail.com>
2026-03-09 20:23:43 +0300

    Update gui.c

--- src/gui.c
+++ src/gui.c
@@ -4,37 +4,23 @@
 extern void vga_clear();
 extern void vga_put_char(char c, uint8_t attr, int x, int y);
 
-#define ATTR_SEL  0x70
-#define ATTR_NORM 0x07
-#define MAX_ITEMS 3
+void handle_key(uint8_t sc);
+void draw_menu();
 
-const char* menu[] = {
-    "Start Shell",
-    "Reboot",
-    "Shutdown"
-};
-
-int selected = 0;
+int sel = 0;
 
 void draw_menu() {
     vga_clear();
-    for (int i = 0; i < MAX_ITEMS; i++) {
-        uint8_t attr = (i == selected) ? ATTR_SEL : ATTR_NORM;
-        vga_put_char('>', attr, 2, 5 + i);
-        for (int j = 0; j < strlen(menu[i]); j++) {
-            vga_put_char(menu[i][j], attr, 4 + j, 5 + i);
-        }
+    const char* m[] = {"Start", "Reboot", "Exit"};
+    for (int i = 0; i < 3; i++) {
+        uint8_t a = (i == sel) ? 0x70 : 0x07;
+        vga_put_char('>', a, 2, 5+i);
+        for (int j = 0; j < strlen(m[i]); j++)
+            vga_put_char(m[i][j], a, 4+j, 5+i);
     }
 }
 
-void handle_key(uint8_t scancode) {
-    if (scancode == 0x48) { // ↑
-        selected = (selected - 1 + MAX_ITEMS) % MAX_ITEMS;
-        draw_menu();
-    } else if (scancode == 0x50) { // ↓
-        selected = (selected + 1) % MAX_ITEMS;
-        draw_menu();
-    } else if (scancode == 0x1C) { // Enter
-        // ничего не делаем — пока
-    }
+void handle_key(uint8_t sc) {
+    if (sc == 0x48) { sel = (sel+2)%3; draw_menu(); }
+    if (sc == 0x50) { sel = (sel+1)%3; draw_menu(); }
 }
