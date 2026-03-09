0276d27b528283e1eebbd4a41363def21bec6946
dringib1-hub <dringib1@gmail.com>
2026-03-09 20:20:47 +0300

    Update files.c

--- src/files.c
+++ src/files.c
@@ -1,34 +1,14 @@
 #include <stdint.h>
 #include <stddef.h>
-#include <string.h>  // ← добавь эту строку
+#include <string.h>
 
 void memset(void* dst, uint8_t val, size_t n) {
     uint8_t* p = (uint8_t*)dst;
     while (n--) *p++ = val;
 }
 
-void memcpy(void* dst, const void* src, size_t n) {
-    uint8_t* d = (uint8_t*)dst;
-    const uint8_t* s = (const uint8_t*)src;
-    while (n--) *d++ = *s++;
-}
-
 int strlen(const char* s) {
-    int len = 0;
-    while (s[len]) len++;
-    return len;
-}
-
-void itoa(int n, char* buf, int base) {
-    static char digits[] = "0123456789ABCDEF";
     int i = 0;
-    if (n < 0) { buf[i++] = '-'; n = -n; }
-    do {
-        buf[i++] = digits[n % base];
-        n /= base;
-    } while (n);
-    buf[i] = 0;
-    for (int l = 0, r = i-1; l < r; l++, r--) {
-        char t = buf[l]; buf[l] = buf[r]; buf[r] = t;
-    }
+    while (s[i]) i++;
+    return i;
 }
