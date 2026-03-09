a60fd38fc8f4422c4f7b8f30026f5f6024830c7e
dringib1-hub <dringib1@gmail.com>
2026-03-09 20:19:42 +0300

    Update drivers.asm

--- src/drivers.asm
+++ src/drivers.asm
@@ -1,44 +1,14 @@
 [BITS 32]
+global irq1_handler
+extern handle_key
 
-extern keyboard_handler
-extern vga_clear
-extern vga_put_char
-
-; IRQ1 — клавиатура
-irq1:
+irq1_handler:
     pusha
     in al, 0x60
     push eax
-    call keyboard_handler
+    call handle_key
     pop eax
     mov al, 0x20
     out 0x20, al
     popa
     iret
-
-; VGA text mode (0xB8000)
-vga_clear:
-    pusha
-    mov edi, 0xB8000
-    mov ecx, 80 * 25
-    mov eax, 0x0720   ; attr=07, char=20 (space)
-    rep stosd
-    popa
-    ret
-
-vga_put_char:
-    pusha
-    mov ebx, 0xB8000
-    mov eax, [esp+36]   ; char
-    mov edx, [esp+40]   ; attr
-    mov ecx, [esp+44]   ; x
-    mov esi, [esp+48]   ; y
-    mov edi, esi
-    mov eax, 80
-    mul edi
-    add eax, ecx
-    shl eax, 1
-    add ebx, eax
-    mov [ebx], dx
-    popa
-    ret
